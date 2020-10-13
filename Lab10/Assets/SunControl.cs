ca54657657b957a8c1d129870e7f42effe9c888a
DESKTOP-T6PM49F\rebor <thitipong_janjom@cmu.ac.th>
2020-10-14 03:06:34 +0700

    Finish

---
+++ Lab10/Assets/SunControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunControl : MonoBehaviour
+{
+    [SerializeField] private float sunRotationDegreePerSecond = 30.0f;
+
+    private Light sun;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sun = this.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sun.transform.Rotate(new Vector3(0, 1, 0), this.sunRotationDegreePerSecond * Time.deltaTime);
+    }
+}
